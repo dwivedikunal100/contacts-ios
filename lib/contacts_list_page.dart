a4b5ece64481cb38c60adc6a3a7688458c375cbd
Kunal Dwivedi <dwikunal@amazon.com>
2020-09-27 13:02:14 +0530

    More commit

---
+++ lib/contacts_list_page.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
+
+class ContactListPage extends StatefulWidget {
+  @override
+  _ContactListPageState createState() => _ContactListPageState();
+}
+
+class _ContactListPageState extends State<ContactListPage> {
+  List<Contact> _contacts;
+
+  get iOSLocalizedLabels => true;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshContacts();
+  }
+
+  Future<void> refreshContacts() async {
+    var contacts = (await ContactsService.getContacts()).toList();
+    setState(() {
+      _contacts = contacts;
+    });
+  }
+
+  void updateContact() async {
+    Contact ninja = _contacts
+        .toList()
+        .firstWhere((contact) => contact.familyName.startsWith("Ninja"));
+    ninja.avatar = null;
+    await ContactsService.updateContact(ninja);
+
+    refreshContacts();
+  }
+
+  _openContactForm() async {
+    try {
+      var contact = await ContactsService.openContactForm(
+          iOSLocalizedLabels: iOSLocalizedLabels);
+      refreshContacts();
+    } on FormOperationException catch (e) {
+      switch (e.errorCode) {
+        case FormOperationErrorCode.FORM_OPERATION_CANCELED:
+        case FormOperationErrorCode.FORM_COULD_NOT_BE_OPEN:
+        case FormOperationErrorCode.FORM_OPERATION_UNKNOWN_ERROR:
+        default:
+          print(e.errorCode);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Contacts Manager',
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.create),
+            onPressed: null,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add), onPressed: _openContactForm),
+      body: SafeArea(
+        child: _contacts != null
+            ? ListView.builder(
+                itemCount: _contacts?.length ?? 0,
+                itemBuilder: (BuildContext context, int index) {
+                  Contact c = _contacts?.elementAt(index);
+                  return ListTile(
+                    onTap: () {
+                      var temp = Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) => ContactDetailsPage(
+                                c,
+                                onContactDeviceSave:
+                                    contactOnDeviceHasBeenUpdated,
+                              )));
+                      temp.then((value) => refreshContacts());
+                    },
+                    leading: (c.avatar != null && c.avatar.length > 0)
+                        ? CircleAvatar(backgroundImage: MemoryImage(c.avatar))
+                        : CircleAvatar(child: Text(c.initials())),
+                    title: Text(c.displayName ?? ""),
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+    );
+  }
+
+  void contactOnDeviceHasBeenUpdated(Contact contact) {
+    this.setState(() {
+      var id = _contacts.indexWhere((c) => c.identifier == contact.identifier);
+      _contacts[id] = contact;
+    });
+  }
+}
+
+class ContactDetailsPage extends StatelessWidget {
+  ContactDetailsPage(this._contact, {this.onContactDeviceSave});
+
+  final Contact _contact;
+  final Function(Contact) onContactDeviceSave;
+
+  get iOSLocalizedLabels => true;
+
+  _openExistingContactOnDevice(BuildContext context) async {
+    try {
+      var contact = await ContactsService.openExistingContact(_contact,
+          iOSLocalizedLabels: iOSLocalizedLabels);
+      if (onContactDeviceSave != null) {
+        onContactDeviceSave(contact);
+      }
+      Navigator.of(context).pop();
+    } on FormOperationException catch (e) {
+      switch (e.errorCode) {
+        case FormOperationErrorCode.FORM_OPERATION_CANCELED:
+        case FormOperationErrorCode.FORM_COULD_NOT_BE_OPEN:
+        case FormOperationErrorCode.FORM_OPERATION_UNKNOWN_ERROR:
+        default:
+          print(e.toString());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_contact.displayName ?? ""),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              ContactsService.deleteContact(_contact);
+              Navigator.pop(context);
+            },
+          ),
+          IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () => _openExistingContactOnDevice(context)),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Name"),
+              trailing: Text(_contact.givenName ?? ""),
+            ),
+            ListTile(
+              title: Text("Middle name"),
+              trailing: Text(_contact.middleName ?? ""),
+            ),
+            ListTile(
+              title: Text("Family name"),
+              trailing: Text(_contact.familyName ?? ""),
+            ),
+            ListTile(
+              title: Text("Prefix"),
+              trailing: Text(_contact.prefix ?? ""),
+            ),
+            ListTile(
+              title: Text("Suffix"),
+              trailing: Text(_contact.suffix ?? ""),
+            ),
+            ListTile(
+              title: Text("Birthday"),
+              trailing: Text(_contact.birthday != null
+                  ? _contact.birthday.toString()
+                  : ""),
+            ),
+            ListTile(
+              title: Text("Company"),
+              trailing: Text(_contact.company ?? ""),
+            ),
+            ListTile(
+              title: Text("Job"),
+              trailing: Text(_contact.jobTitle ?? ""),
+            ),
+            ListTile(
+              title: Text("Account Type"),
+              trailing: Text((_contact.androidAccountType != null)
+                  ? _contact.androidAccountType.toString()
+                  : ""),
+            ),
+            AddressesTile(_contact.postalAddresses),
+            ItemsTile("Phones", _contact.phones),
+            ItemsTile("Emails", _contact.emails)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddressesTile extends StatelessWidget {
+  AddressesTile(this._addresses);
+
+  final Iterable<PostalAddress> _addresses;
+
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ListTile(title: Text("Addresses")),
+        Column(
+          children: _addresses
+              .map((a) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text("Street"),
+                          trailing: Text(a.street ?? ""),
+                        ),
+                        ListTile(
+                          title: Text("Postcode"),
+                          trailing: Text(a.postcode ?? ""),
+                        ),
+                        ListTile(
+                          title: Text("City"),
+                          trailing: Text(a.city ?? ""),
+                        ),
+                        ListTile(
+                          title: Text("Region"),
+                          trailing: Text(a.region ?? ""),
+                        ),
+                        ListTile(
+                          title: Text("Country"),
+                          trailing: Text(a.country ?? ""),
+                        ),
+                      ],
+                    ),
+                  ))
+              .toList(),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemsTile extends StatelessWidget {
+  ItemsTile(this._title, this._items);
+
+  final Iterable<Item> _items;
+  final String _title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ListTile(title: Text(_title)),
+        Column(
+          children: _items
+              .map(
+                (i) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: ListTile(
+                    title: Text(i.label ?? ""),
+                    trailing: Text(i.value ?? ""),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:contactsios/detail_page.dart';
+import 'package:contactsios/contacts_list_page.dart';
 import 'package:flutter/material.dart';
-import 'package:contacts_service/contacts_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,105 +12,8 @@ class MyApp extends StatelessWidget {
       title: 'Contacts Manager',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Contacts Manager'),
+      home: ContactListPage(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool value = false;
-
-  void _incrementCounter() {
-    setState(() {
-      value = true;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    getContacts();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          MaterialButton(
-            padding: EdgeInsets.all(8.0),
-            minWidth: 0,
-            child: Icon(
-              Icons.edit,
-              color: Colors.white,
-            ),
-            onPressed: null,
-          ),
-          MaterialButton(
-            padding: EdgeInsets.all(8.0),
-            minWidth: 0,
-            child: Icon(
-              Icons.exit_to_app,
-              color: Colors.white,
-            ),
-            onPressed: () => null,
-          ),
-        ],
-      ),
-      body: Container(
-        child: buildContacts(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  Future<List<Contact>> getContacts() async {
-    Iterable<Contact> contacts =
-        await ContactsService.getContacts(withThumbnails: false);
-    return contacts.toList();
-  }
-
-  FutureBuilder buildContacts() {
-    return FutureBuilder(
-        future: getContacts(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Contact c = snapshot.data[index];
-                  return Column(children: [
-                    ListTile(
-                      leading: (c.avatar != null && c.avatar.length > 0)
-                          ? CircleAvatar(backgroundImage: MemoryImage(c.avatar))
-                          : CircleAvatar(child: Text(c.initials())),
-                      title: Text(snapshot.data[index].displayName),
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) =>
-                                DetailPage(contact: snapshot.data[index])),
-                      ),
-                    ),
-                    Divider()
-                  ]);
-                });
-          } else {
-            return CircularProgressIndicator();
-          }
-        });
-  }
-
-// flutter emulator --launch apple_ios_simulator
 }
